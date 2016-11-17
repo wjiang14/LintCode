fc2eb8e0126877a7f7601472e91af2e1f38c5d16
wjiang14 <jwjovi@gmail.com>
2016-11-16 19:40:44 -0500

    Dynamic Programming

---
+++ LinCode_class4/Unique_Paths.cpp
@@ -0,0 +1,27 @@
+// Unique Paths
+
+// A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+// The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+// How many possible unique paths are there?
+// 滚动数组
+
+class Solution {
+public:
+    /**
+     * @param n, m: positive integer (1 <= n ,m <= 100)
+     * @return an integer
+     */
+    int uniquePaths(int m, int n) {
+        // wirte your code here
+        if (m < 0 || n < 0) {
+            return 0;
+        }
+        vector<int> dp(n, 1);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[j] += dp[j - 1];
+            }
+        }
+        return dp.back();
+    }
+};
